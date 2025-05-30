9a395133c9e23814d2f410699c3556dd30d9f837
José Sáenz <me@josedante.com>
2025-05-30 15:52:10 -0500

    feat: agregar configuración de Tailwind CSS y estructura de componentes en Nuxt.js

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,27 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
   ],
+  css: ["~/assets/css/main.css"],
 
   runtimeConfig: {
     // Private keys (only available on server-side)
-    apiBase: process.env.NUXT_PRIVATE_API_BASE || 'http://localhost:8000',
-    
+    apiBase: process.env.NUXT_PRIVATE_API_BASE || "http://localhost:8000",
+
     // Public keys (exposed to client-side)
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
-      isDevelopment: process.env.NODE_ENV === 'development'
-    }
-  }
-})
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000",
+      isDevelopment: process.env.NODE_ENV === "development",
+    },
+  },
+});
